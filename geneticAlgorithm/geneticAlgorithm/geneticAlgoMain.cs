2a8a203280639a80b918d63e4db1a28f10e41132
reidtc82 <reidtc82@gmail.com>
2013-09-17 23:03:56 -0500

    Added main class
    output to debug console

---
+++ geneticAlgorithm/geneticAlgorithm/geneticAlgoMain.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace geneticAlgorithm
+{
+    class geneticAlgoMain
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello world!");
+        }
+    }
+}
